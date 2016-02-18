04bf0c832a73272c233db5249333786c99f62e1c
Andreas Niedermair <andreas@niedermair.name>
2016-02-18 15:46:08 +0100

    fixed assembly title and product

--- UDPBroadcast.Net40/Properties/AssemblyInfo.cs
+++ UDPBroadcast.Net40/Properties/AssemblyInfo.cs
@@ -5,11 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("UDPBroadcast.Net40")]
+[assembly: AssemblyTitle("UDPBroadcast")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("UDPBroadcast.Net40")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyProduct("UDPBroadcast")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
