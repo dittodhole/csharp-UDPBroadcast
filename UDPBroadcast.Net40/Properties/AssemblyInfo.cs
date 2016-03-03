4d5ffc2197dce37d7c5c2648d1496c71fc4da52a
Andreas Niedermair <andreas@niedermair.name>
2016-03-03 22:01:16 +0100

    unified assembly attribution

--- UDPBroadcast.Net40/Properties/AssemblyInfo.cs
+++ UDPBroadcast.Net40/Properties/AssemblyInfo.cs
@@ -1,34 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
 [assembly: AssemblyProduct("UDPBroadcast")]
 [assembly: AssemblyCompany("Andreas Niedermair")]
 [assembly: AssemblyDescription("UDPBroadcast aims to provide a simple interface to broadcast messages within the local network over UDP.")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
 [assembly: Guid("d88b63de-a902-4eaa-8d39-7314a52ecd28")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
